5561b052246387dcca5d261f854ad83fc38a7ba8
Sam de Jong <samdejong86@gmail.com>
2018-03-07 12:36:47 -0800

    Can now send a single line to the device

--- AgilentControl.py
+++ AgilentControl.py
@@ -4,10 +4,15 @@ import argparse
 
 #parse arguments
 parser = argparse.ArgumentParser(description='Send a list of messages to an Agilent 33600 AWG')
-parser.add_argument('-f','--filename', help='Macro file containing SCPI commands', default="./squareWave.awg", required=True)
+parser.add_argument('-f','--filename', help='Macro file containing SCPI commands', default="", required=False)
 parser.add_argument('-a','--address', help="Address of device", default="142.104.60.122", required=False)
+parser.add_argument('-l', '--line', help="A single command to send to the device", default="", required=False)
 
 args = parser.parse_args()
+
+if args.line == "" and args.filename == "":
+    parser.error("either --line or --filename must be specified")
+    
 
 
 #load the VISA resource manager
@@ -26,13 +31,19 @@ inst.write("DISP:TEXT '"+message+"'")
 inst.write("SOURCE2:FREQUENCY:MODE FIX")
 
 #sent commands to device
-for line in open(args.filename):
-    sleep(0.1)
-    #ignore commented lines
-    if line[0] == '#':
-        continue;
-    inst.write(line.strip())
-    print(line.strip())
+
+if args.line !="":
+    inst.write(args.line)
+    print(args.line)
+
+if args.filename != "":
+    for line in open(args.filename):
+        sleep(0.1)
+        #ignore commented lines
+        if line[0] == '#':
+            continue;
+        inst.write(line.strip())
+        print(line.strip())
 
 
 inst.write("DISP:TEXT ''")
